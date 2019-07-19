13ed6fbff1929b0c9a08fee57a549987b02832bd
René Winkler <info@rewiso.ch>
2019-07-19 17:33:06 +0200

    use JpaRepository

--- src/main/java/ch/rewiso/graphqlbooks/repository/AuthorRepository.java
+++ src/main/java/ch/rewiso/graphqlbooks/repository/AuthorRepository.java
@@ -1,8 +1,8 @@
 package ch.rewiso.graphqlbooks.repository;
 
 import ch.rewiso.graphqlbooks.domain.Author;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AuthorRepository extends CrudRepository<Author, Long> {
+public interface AuthorRepository extends JpaRepository<Author, Long> {
 
 }
